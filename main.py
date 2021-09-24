1f90a9b43829a226d116ce1d317f190909db4fe1
Gavin Rohrer <gavin@rohrer.org>
2021-09-23 22:47:27 -0400

    switch from sqlite3 package to databases package, should mean better cross platform

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ import feedparser
 import requests
 import json
 import re
-import sqlite3
 from datetime import datetime
 import sys
 import time
 import random
+from databases import Database
+import asyncio
 
 with open("config.json") as config_file:
 	config = json.load(config_file)
@@ -16,12 +17,17 @@ with open("config.json") as config_file:
 hook_num = len(config["webhooks"])
 
 query_count = 0
-DEBUG = True
+DEBUG = False
 
 
 def gprint(message):
 	if DEBUG:
 		print(message)
+
+
+
+database = Database(f'sqlite:///{config["db_file"]}')
+
 
 # timing different calls
 timer = 0
@@ -32,27 +38,18 @@ def timer_end():
 	return time.time() - timer
 
 timer_start()
+asyncio.run(database.connect())
+gprint("Database init took %3.2f"%(timer_end()))
+
+timer_start()
 d = feedparser.parse(config["feed"])
 gprint("Feed fetch took %3.2f"%(timer_end()))
 
 
-def create_connection(db_file):
-	""" create a database connection to the SQLite database
-		specified by db_file
-	:param db_file: database file
-	:return: Connection object or None
-	"""
-	conn = None
-	try:
-		conn = sqlite3.connect(db_file)
-		return conn
-	except Exception as e:
-		raise Exception(f"Connection creation error: {e}")
 
-	return conn
 
 wait_cache = []
-def get_waits(conn):
+async def get_waits():
 	global wait_cache
 	global query_count
 
@@ -61,15 +58,13 @@ def get_waits(conn):
 
 	
 	query = "SELECT * FROM waits"
-	c = conn.cursor()
-	c.execute(query)
-	rows = c.fetchall()
+	rows = await database.fetch_all(query=query)
 	query_count += 1
 	wait_cache = rows
 	# out = tuple((x[1] for x in rows))
 	return rows
 
-def create_tables(conn):
+async def create_tables():
 	global query_count
 	timer_start()
 	query1 = """ CREATE TABLE IF NOT EXISTS previous (
@@ -87,16 +82,12 @@ def create_tables(conn):
 									); """
 
 
-	if conn is None:
-		raise ("conn is None in table creation")
 
 	try:
-		c = conn.cursor()
-		c.execute(query1)
+		await database.execute(query=query1)
 		query_count += 1
 
-		c = conn.cursor()
-		c.execute(query2)
+		await database.execute(query=query2)
 		query_count += 1
 
 	except Exception as e:
@@ -105,87 +96,78 @@ def create_tables(conn):
 
 	rows = []
 	try:
-		rows = get_waits(conn)
+		rows = await get_waits()
 	except:
 		gprint("wait rows get error")
 
 
 	if not len(rows) == len(config["run_wait"]):
-		c = conn.cursor()
-		c.execute("DELETE FROM waits")
-		conn.commit()
+		await database.execute("DELETE FROM waits")
 		query_count += 1
 		for i in config["run_wait"]:
-			c = conn.cursor()
-			c.execute("INSERT INTO waits(count, initial) VALUES(1, ?)", (i,))
+			await database.execute(query="INSERT INTO waits(count, initial) VALUES(1, :initial)", values={'initial':i})
 			query_count += 1
-		conn.commit()
 	gprint("table init finished in %3.2f" %(timer_end()))
 
 unq_cached_hook = -1
 unq_cache = []
-def verify_unique(conn, reddit_id, hook_id):
+async def verify_unique(reddit_id, hook_id):
 	global query_count
 	global unq_cache
 	global unq_cached_hook
 
 	if unq_cached_hook != hook_id:
-		query = "SELECT reddit_id FROM previous WHERE hook_id=? ORDER BY id DESC;"
-		cur = conn.cursor()
-		cur.execute(query, (hook_id,))
-		unq_cached_hook = hook_id
 
 		# this should always work - I believe reddit rss has no more than 25 entries
 		# since the order is id DESC, it grabs the most recent several
 		# if there is any duplicates, this line is the culprit
-		unq_cache = cur.fetchmany(60)
+		query = "SELECT reddit_id FROM previous WHERE hook_id=:hook_id ORDER BY id DESC LIMIT 200;"
+		unq_cache = await database.fetch_all(query=query, values={'hook_id': hook_id})
 
+		unq_cached_hook = hook_id
 
 		query_count += 1
 
 
 	# sqlite fetch returns tuples, so we need to search the tuples
 	if (reddit_id,) in unq_cache:
+		gprint(f"{reddit_id} is not unique for hook {hook_id}")
 		raise Exception(f"{reddit_id} is not unique for hook {hook_id}")
 	else:
 		return True
 
-def add_post(conn, reddit_id, media_lnk, hook_id):
+async def add_post(reddit_id, media_lnk, hook_id):
 	global query_count
 	query  = '''INSERT INTO previous(reddit_id,media_lnk,date_sent,hook_id)
-				VALUES(?,?,?,?) '''
+				VALUES(:reddit_id,:media_lnk,:date_sent,:hook_id) '''
 
 	date = datetime.now().strftime("%F")
 
-	cur = conn.cursor()
-	cur.execute(query, (reddit_id, media_lnk, date, hook_id))
-	conn.commit()
+	await database.execute(query=query, values={'reddit_id': reddit_id, 'media_lnk':media_lnk, 'date_sent':date, 'hook_id':hook_id})
 	query_count += 1
 
-	return cur.lastrowid
+	return None
 
-def decrement_waits(conn):
+async def decrement_waits():
 	global query_count
-	current = get_waits(conn)
+	current = await get_waits()
 	for i in current:
-		c = conn.cursor()
 
 		if i[2] == 1:
 			continue
 
 		if i[1] == 1:
-			c.execute("UPDATE waits SET count = ? WHERE id = ?", (i[2], i[0]))
+			await database.execute(query="UPDATE waits SET count = :count WHERE id = :id", values={'count':i[2], 'id':i[0]})
 			gprint(f"resetting id {i[0] - 1}")
 		else:
-			c.execute("UPDATE waits SET count = ? WHERE id = ?", (i[1] - 1, i[0]))
+			await database.execute(query="UPDATE waits SET count = :count WHERE id = :id", values={'count':i[1] - 1, 'id':i[0]})
 		query_count += 1
-	conn.commit()
 
-def verify_timing(conn, hook_index):
+async def verify_timing(hook_index):
 	# the "run_wait" setting means that we only want to send to the webhook corresponding to the index 
 	# every that many times the program is run 
 	initial = config["run_wait"]
-	current = get_waits(conn)
+	current = await get_waits()
 	out = current[hook_index][1] == 1
 	return out
 
@@ -224,7 +206,7 @@ def send(media_lnk, hook_index):
 	time.sleep(config["request_pause"])
 	gprint("Sent image to webhook %i in %3.2f" %(hook_index, timer_end()))
 
-def find_entry(conn, hook_id):
+async def find_entry(hook_id):
 	for entry in d["entries"]:
 
 		# I'm not sure what I think of this here - it will be called again after this function
@@ -232,9 +214,10 @@ def find_entry(conn, hook_id):
 		entry_id = get_id_from_entry(entry)
 		
 		try:
-			verify_unique(conn, entry_id, hook_id)
+			await verify_unique(entry_id, hook_id)
 			get_image_from_entry(entry)
-		except Exception:
+		except Exception as e:
+			gprint(str(e))
 			continue
 
 		return entry
@@ -248,33 +231,32 @@ class gcolor:
 
 
 
-def main():
+async def main():
 
 
-	conn = create_connection(config["db_file"])
-	create_tables(conn)
+	await create_tables()
 
 	complete = [False] * hook_num
 
 	for i in range(hook_num):
 
 		# don't bother finding a post to send if it's not time
-		if not verify_timing(conn, i):
+		if not await verify_timing(i):
 			continue
 
 		try:
-			entry = find_entry(conn, i)
+			entry = await find_entry(i)
 			entry_id = get_id_from_entry(entry)
 			media_lnk = get_image_from_entry(entry)
 
 			send(media_lnk, i)
 
-			add_post(conn, entry_id, media_lnk, i)
+			await add_post(entry_id, media_lnk, i)
 
 		except Exception as e: print(f"{gcolor.FAIL}POST FAILURE: {e}{gcolor.ENDC}")
 
 	try:
-		decrement_waits(conn)
+		await decrement_waits()
 	except Exception as e: print(f"{gcolor.FAIL}Decriment failure: {e}\n\tThis will cause unsatisfactory post volumes{gcolor.ENDC}")
 
 	print(f"finished having made {gcolor.NUMBER}{query_count}{gcolor.ENDC} queries in total")
@@ -283,7 +265,7 @@ def main():
 
 	
 
-main()
+asyncio.run(main())
 
 
 
